4f7ca04f04c21c84597139348c891c0a7927fb2b
noexanu <solonovichyan@gmail.com>
2020-10-03 13:02:22 +0300

    feat: add min max avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,44 @@
-
 exports.min = function min (array) {
-  return 0;
+	if (arguments.length === 0 || array.length === 0) {
+		return 0;
+	} else {
+		let minimum = array[0];
+		for (element of array) {
+			if (element < minimum) {
+				minimum = element
+			}
+		}
+		return minimum;
+	}
 }
+
+
 
 exports.max = function max (array) {
-  return 0;
+	if (arguments.length === 0 || array.length === 0) {
+		return 0;
+	} else {
+		let maximum = array[0];
+		for (element of array) {
+			if (element > maximum) {
+				maximum = element
+			}
+		}
+		return maximum;
+	}
 }
 
+
+
 exports.avg = function avg (array) {
-  return 0;
+	if (arguments.length === 0 || array.length === 0) {
+		return 0;
+	} else {
+		let average = 0;
+		for (element of array) {
+			average += element;
+		}
+		average /= array.length;
+		return average;
+	}
 }
